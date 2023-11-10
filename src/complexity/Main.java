5df23a82a3ae714676b17aeef07df9dd8044bcb0
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-10 23:14:33 +0200

    Droping non-dominant term

--- src/complexity/Main.java
+++ src/complexity/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 //        System.out.println(multiplyNumbers(100));
 //        printNumbers(100);
-        printNumbers2(10);
+//        printNumbers2(10);
+        dropNonDominantTerm(100);
     }
 
     // O(1)
@@ -26,6 +27,19 @@ public class Main {
             for (int j = 0; j < n; j++) {
                 System.out.println(i + " " + j);
             }
+        }
+    }
+
+    // O(n^2) drop non-dominant term
+    private static void dropNonDominantTerm(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
         }
     }
 }
