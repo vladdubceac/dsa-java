cd2ad15704c36abc4e612abbe385244d14cd8a72
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-10 23:00:43 +0200

    Adding O(n) example

--- src/complexity/Main.java
+++ src/complexity/Main.java
@@ -3,11 +3,19 @@ package complexity;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(multiplyNumbers(100));
+//        System.out.println(multiplyNumbers(100));
+        printNumbers(100);
     }
 
-    // O(1) complexity example
+    // O(1)
     private static int multiplyNumbers(int n) {
-        return n*n;
+        return n * n;
+    }
+
+    // O(n)
+    private static void printNumbers(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
     }
 }
