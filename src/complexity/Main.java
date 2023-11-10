e0c3725b066c01359b15bfe8e34c5037f963e53c
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-10 23:04:40 +0200

    N-squared complexity

--- src/complexity/Main.java
+++ src/complexity/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 //        System.out.println(multiplyNumbers(100));
-        printNumbers(100);
+//        printNumbers(100);
+        printNumbers2(10);
     }
 
     // O(1)
@@ -16,6 +17,15 @@ public class Main {
     private static void printNumbers(int n) {
         for (int i = 0; i < n; i++) {
             System.out.println(i);
+        }
+    }
+
+    // O(n^2)
+    private static void printNumbers2(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
         }
     }
 }
