0c84e7288c9cec9d74a77382e838df59eee6dc3f
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-10 23:34:18 +0200

    Space complexity

--- src/complexity/Main.java
+++ src/complexity/Main.java
@@ -6,7 +6,8 @@ public class Main {
 //        System.out.println(multiplyNumbers(100));
 //        printNumbers(100);
 //        printNumbers2(10);
-        dropNonDominantTerm(100);
+//        dropNonDominantTerm(100);
+        System.out.println(sumNumbersRecursively(10));
     }
 
     // O(1)
@@ -41,5 +42,47 @@ public class Main {
         for (int i = 0; i < n; i++) {
             System.out.println(i);
         }
+    }
+
+    // O(a+b)
+    private static void printNumbers(int a, int b) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(i);
+        }
+
+        for (int j = 0; j < b; j++) {
+            System.out.println(j);
+        }
+    }
+
+    // O(a*b)
+    private static void printNumbers2(int a, int b) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
+    }
+
+    // O(n)
+    private static int sumNumbersRecursively(int n) {
+        System.out.println("\t" + n);
+        if (n <= 0) {
+            return 0;
+        }
+        return n + sumNumbersRecursively(n - 1);
+    }
+
+    // Space complexity O(1)
+    public static int pairSumSequence(int n) {
+        var total = 0;
+        for (int i = 0; i <= n; i++) {
+            total = total + pairSum(i, i + 1);
+        }
+        return total;
+    }
+
+    public static int pairSum(int a, int b) {
+        return a + b;
     }
 }
