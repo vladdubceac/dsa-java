b3dd6d51d65721c530c0d3f602716bf7c81edd98
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-11 22:10:45 +0200

    Insertion in Array

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -15,5 +15,12 @@ public class Main {
 
         String sArray[] = {"a","b","c"};
         System.out.println(Arrays.toString(sArray));
+
+        SingleDimensionArray sda = new SingleDimensionArray(10);
+        sda.insert(0,0);
+        sda.insert(1,10);
+        sda.insert(2,20);
+        sda.insert(1,30);
+        sda.insert(12,120);
     }
 }
---
+++ src/arrays/SingleDimensionArray.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class SingleDimensionArray {
+    int arr[] = null;
+
+    public SingleDimensionArray(int sizeOfArray) {
+        arr = new int[sizeOfArray];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.MIN_VALUE;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void insert(int location, int valueToBeInserted) {
+        try {
+            if (arr[location] == Integer.MIN_VALUE) {
+                arr[location] = valueToBeInserted;
+                System.out.println("Successfully inserted!");
+            } else {
+                System.out.println("This cell is already occupied");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index to access array ! ");
+        }
+    }
+}
