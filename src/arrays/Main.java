56d59f29725cf386b20d65ecd89484cdeae37072
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-11 21:53:24 +0200

    Creating an array

---
+++ src/arrays/Main.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray;
+        intArray = new int[3];
+        intArray[0] = 1;
+        intArray[1] = 2;
+        intArray[2] = 3;
+        System.out.println(Arrays.toString(intArray));
+
+        // All together
+
+        String sArray[] = {"a","b","c"};
+        System.out.println(Arrays.toString(sArray));
+    }
+}
