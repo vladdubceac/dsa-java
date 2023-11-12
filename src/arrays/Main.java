58b2cd6f5b65ce78c5314a8063575b074eb7874c
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-12 12:03:35 +0200

    Accessing elements in array

--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -10,17 +10,31 @@ public class Main {
         intArray[1] = 2;
         intArray[2] = 3;
         System.out.println(Arrays.toString(intArray));
-
+        System.out.println("----------");
         // All together
 
         String sArray[] = {"a","b","c"};
         System.out.println(Arrays.toString(sArray));
+        System.out.println("----------");
 
-        SingleDimensionArray sda = new SingleDimensionArray(10);
-        sda.insert(0,0);
-        sda.insert(1,10);
-        sda.insert(2,20);
-        sda.insert(1,30);
-        sda.insert(12,120);
+        SingleDimensionArray sda = new SingleDimensionArray(3);
+        sda.insert(0, 0);
+        sda.insert(1, 10);
+        sda.insert(2, 20);
+//        sda.insert(1,30);
+//        sda.insert(12,120);
+        System.out.println("----------");
+
+        var firstElement = sda.arr[0];
+        System.out.println(firstElement);
+        var thirdElement = sda.arr[2];
+        System.out.println(thirdElement);
+        try {
+            var illegalElement = sda.arr[3];
+            System.out.println(illegalElement);
+        }catch (Exception e){
+           e.printStackTrace();
+        }
+        System.out.println("----------");
     }
 }
