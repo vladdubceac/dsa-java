0f46d2475ec196166d9cf28fb6ff85f0522c1918
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-18 14:11:56 +0200

    Find the days above average temperature

--- src/arrays/project/Main.java
+++ src/arrays/project/Main.java
@@ -7,15 +7,25 @@ public class Main {
         Scanner console = new Scanner(System.in);
         System.out.println("How many days' temperatures ? ");
         int numDays = console.nextInt();
+        int[] temps = new int[numDays];
+        // record temperatures and find average
         int sum = 0;
-        for (int i = 1; i <= numDays; i++) {
-            System.out.print("Day " + i + "'s high temp : ");
-            int next = console.nextInt();
-            sum += next;
+        for (int i = 0; i < numDays; i++) {
+            System.out.print("Day " + (i+1) + "'s high temp : ");
+            temps[i] = console.nextInt();
+            sum += temps[i];
         }
 
-        double average = sum / numDays;
+        double average = (double)sum / numDays;
+        // count days above average
+        int above = 0;
+        for(int i=0;i<temps.length;i++){
+            if(temps[i]>average){
+                 above++;
+            }
+        }
         System.out.println();
         System.out.println("Average Temp = " + average);
+        System.out.println(above+" days above average temperature " + average);
     }
 }
