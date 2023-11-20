7d41c2f846eb4ec60c96d728004de07a2d858398
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-20 20:35:16 +0200

    ArrayList. Creating ArrayList

---
+++ src/array_lists/Creation.java
@@ -0,0 +1,11 @@
+package array_lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Creation {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(30,10,20));
+        System.out.println(numbers);
+    }
+}
