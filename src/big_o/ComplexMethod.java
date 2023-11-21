06dbd08041370f7815bcec9d0378d7e9c38bd324
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-11-21 20:25:42 +0200

    Big-O questions. Nested loops complexity

---
+++ src/big_o/ComplexMethod.java
@@ -0,0 +1,19 @@
+package big_o;
+
+public class ComplexMethod {
+    public static void main(String[] args) {
+        int[] arrayA = {1,2,3,4,5};
+        int[] arrayB = {4,5,6,7,8,9};
+        printUnorderedPairs(arrayA,arrayB);
+    }
+
+    private static void printUnorderedPairs(int[] arrayA, int[] arrayB) {
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                for (int k = 0; k < 100_000; k++) {
+                    System.out.println(k+1+". "+arrayA[i]+","+arrayB[j]);
+                }
+            }
+        }
+    }
+}
