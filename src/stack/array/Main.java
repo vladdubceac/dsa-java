d21aeae4ee2fa6f4067f2543b024f0f968094cad
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-12-12 22:42:41 +0200

    Stack using array. Push, pop, peek, delete methods

--- src/stack/array/Main.java
+++ src/stack/array/Main.java
@@ -7,5 +7,20 @@ public class Main {
         System.out.println("Is empty = "+result);
         result = stack.isFull();
         System.out.println("Is full = "+result);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        int top = stack.pop();
+        System.out.println("pop - "+top);
+        System.out.println("peek - "+stack.peek());
+        System.out.println("peek - "+stack.peek());
+        System.out.println("pop - "+stack.pop());
+        System.out.println("peek - "+stack.peek());
+        System.out.println("pop - "+stack.pop());
+        stack.deleteStack();
     }
 }
--- src/stack/array/Stack.java
+++ src/stack/array/Stack.java
@@ -16,10 +16,49 @@ public class Stack {
 
     public boolean isFull(){
         if(topOfStack == arr.length-1){
-            System.out.println("The stack is full");
+//            System.out.println("The stack is full");
             return true;
         }else{
             return false;
         }
+    }
+
+    // Push
+    public void push(int value){
+        if(isFull()){
+            System.out.println("The Stack is full");
+        }else{
+            arr[topOfStack+1]=value;
+            topOfStack++;
+            System.out.println("The value "+value+" is successfully inserted");
+        }
+    }
+
+    // Pop
+    public int pop(){
+        if(isEmpty()){
+            System.out.println("The stack is empty");
+            return Integer.MIN_VALUE;
+        }else {
+            int top= arr[topOfStack];
+            topOfStack--;
+            return top;
+        }
+    }
+
+    // Peek
+    public int peek(){
+        if(isEmpty()){
+            System.out.println("The stack is empty !");
+            return Integer.MIN_VALUE;
+        }else{
+            return arr[topOfStack];
+        }
+    }
+
+    // delete
+    public void deleteStack(){
+        arr = null;
+        System.out.println("The stack is successfully deleted !");
     }
 }
