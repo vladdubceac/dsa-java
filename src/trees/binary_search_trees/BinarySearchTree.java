a434fde58be4f37f2a99ee9c5bf0643afd75f808
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-04 13:06:43 +0200

    Binary search tree. Insert a node to Binary Search Tree

--- src/trees/binary_search_trees/BinarySearchTree.java
+++ src/trees/binary_search_trees/BinarySearchTree.java
@@ -6,4 +6,26 @@ public class BinarySearchTree {
     public BinarySearchTree() {
         root = null;
     }
+
+    // Insert Method
+    private BinaryNode insert(BinaryNode currentNode, int value) {
+        BinaryNode newNode = new BinaryNode();
+        if (currentNode == null) {
+            newNode.setValue(value);
+            System.out.println("The value successfully inserted");
+            return newNode;
+        } else if (value <= currentNode.getValue()) {
+            newNode = insert(currentNode.getLeft(), value);
+            currentNode.setLeft(newNode);
+            return currentNode;
+        }else {
+            newNode = insert(currentNode.getRight(),value);
+            currentNode.setRight(newNode);
+            return currentNode;
+        }
+    }
+
+    void insert(int value){
+        insert(root, value);
+    }
 }
