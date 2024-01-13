3ab83f3e3315f5e45295321c5dee9183b323bea3
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-13 20:52:14 +0200

    Hashing. Direct chaining

---
+++ src/hashing/direct_chaining/Main.java
@@ -0,0 +1,13 @@
+package hashing.direct_chaining;
+
+public class Main {
+    public static void main(String[] args) {
+        DirectChaining directChaining = new DirectChaining(13);
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("quick");
+        directChaining.insertHashTable("brown");
+        directChaining.insertHashTable("fox");
+        directChaining.insertHashTable("over");
+        directChaining.displayHashTable();
+    }
+}
