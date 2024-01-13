ab344f28724f166baffb30bc29a9c05626324a5f
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-13 22:28:49 +0200

    Hashing. Open Addressing - Linear probing. Insert a value

---
+++ src/hashing/open_addressing/linear_probing/LinearProbing.java
@@ -0,0 +1,76 @@
+package hashing.open_addressing.linear_probing;
+
+import java.util.ArrayList;
+
+public class LinearProbing {
+    String[] hashTable;
+    int usedCellNumber;
+
+    public LinearProbing(int size) {
+        hashTable = new String[size];
+        usedCellNumber = 0;
+    }
+
+    public int modASCIIHashFunction(String word, int M) {
+        char ch[];
+        ch = word.toCharArray();
+        int i, sum;
+        for (sum = 0, i = 0; i < word.length(); i++) {
+            sum += ch[i];
+        }
+        return sum % M;
+    }
+
+    public double getLoadFactor() {
+        return usedCellNumber * 1.0 / hashTable.length;
+    }
+
+    // Rehash
+    public void rehashKeys(String word) {
+        usedCellNumber = 0;
+        ArrayList<String> data = new ArrayList<String>();
+        for (String s : hashTable) {
+            if (s != null) {
+                data.add(s);
+            }
+        }
+        data.add(word);
+        hashTable = new String[hashTable.length * 2];
+        for (String s : data) {
+            insertInHashTable(s);
+        }
+    }
+
+    // Insert in HashTable
+    public void insertInHashTable(String word) {
+        double loadFactor = getLoadFactor();
+        if (loadFactor >= 0.75) {
+            rehashKeys(word);
+        } else {
+            int index = modASCIIHashFunction(word, hashTable.length);
+            for (int i = index; i < index + hashTable.length; i++) {
+                int newIndex = i % hashTable.length;
+                if (hashTable[newIndex] == null) {
+                    hashTable[newIndex] = word;
+                    System.out.println("The " + word + " successfully inserted at location: " + newIndex);
+                    break;
+                } else {
+                    System.out.println(newIndex + " is already occupied. Trying next empty cell");
+                }
+            }
+        }
+        usedCellNumber++;
+    }
+
+    public void displayHashTable(){
+        if(hashTable==null){
+            System.out.println("\nHashTable does not exist");
+            return;
+        }else{
+            System.out.println("\n----------HashTable--------");
+            for (int i = 0; i < hashTable.length; i++) {
+                System.out.println("Index "+i+", key: "+hashTable[i]);
+            }
+        }
+    }
+}
---
+++ src/hashing/open_addressing/linear_probing/Main.java
@@ -0,0 +1,13 @@
+package hashing.open_addressing.linear_probing;
+
+public class Main {
+    public static void main(String[] args) {
+        LinearProbing linearProbing = new LinearProbing(13);
+        linearProbing.insertInHashTable("The");
+        linearProbing.insertInHashTable("quick");
+        linearProbing.insertInHashTable("brown");
+        linearProbing.insertInHashTable("fox");
+        linearProbing.insertInHashTable("over");
+        linearProbing.displayHashTable();
+    }
+}
