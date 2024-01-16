a66837d5386abc4a4239d29e7a78e98fe5a97068
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-16 21:32:59 +0200

    Sorting. Selection sort

---
+++ src/sorting/selection_sort/Main.java
@@ -0,0 +1,9 @@
+package sorting.selection_sort;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {10,3,2,5,8,4,1,9};
+        SelectionSort.selectionSort(arr);
+        SelectionSort.printArray(arr);
+    }
+}
