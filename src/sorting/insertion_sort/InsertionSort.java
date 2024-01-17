d345168f329694e87b6aa11bfbfc765eaffdbfb1
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-17 23:39:46 +0200

    Sorting . Insertion sort

---
+++ src/sorting/insertion_sort/InsertionSort.java
@@ -0,0 +1,24 @@
+package sorting.insertion_sort;
+
+public class InsertionSort {
+
+    // Insertion sort
+    static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i;
+            while (j > 0 && array[j - 1] > temp) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/sorting/insertion_sort/Main.java
@@ -0,0 +1,9 @@
+package sorting.insertion_sort;
+
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {10, 8, 2, 5, 7, 4, 3, 1};
+        InsertionSort.insertionSort(arr);
+        InsertionSort.printArray(arr);
+    }
+}
