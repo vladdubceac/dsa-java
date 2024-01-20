a983829a667e03c6789d9e3945bb813ed0f9ce99
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-20 17:17:27 +0200

    Searching . Binary search

--- src/search/Main.java
+++ src/search/Main.java
@@ -3,7 +3,11 @@ package search;
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1,3,2,10,23,11};
-        Searching.linearSearch(arr,10);
-        Searching.linearSearch(arr,20);
+        Searching.linearSearch(arr, 10);
+        Searching.linearSearch(arr, 20);
+
+        arr =new int[]{8, 9, 12, 15, 17, 19, 20, 21, 28};
+        Searching.binarySearch(arr, 15);
+        Searching.binarySearch(arr, 18);
     }
 }
--- src/search/Searching.java
+++ src/search/Searching.java
@@ -11,4 +11,27 @@ public class Searching {
         System.out.println("The element " + value + " not found.");
         return -1;
     }
+
+    // Binary Search
+    public static int binarySearch(int arr[], int value) {
+        int start = 0;
+        int end = arr.length - 1;
+        int middle = (start + end) / 2;
+        while (arr[middle] != value && start <= end) {
+            if (value < arr[middle]) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+            middle = (start + end) / 2;
+        }
+
+        if (arr[middle] == value) {
+            System.out.println("The element " + value + " is found at the index " + middle);
+            return middle;
+        } else {
+            System.out.println("The element " + value + " not found . ");
+            return -1;
+        }
+    }
 }
