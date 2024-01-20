35e9c9dd3ce3e72450b85695a56f29ed44905626
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-20 16:39:19 +0200

    Searching . Linear search

---
+++ src/search/Main.java
@@ -0,0 +1,9 @@
+package search;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1,3,2,10,23,11};
+        Searching.linearSearch(arr,10);
+        Searching.linearSearch(arr,20);
+    }
+}
