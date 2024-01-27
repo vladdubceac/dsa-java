92ade3a3b49ad41da95c124d9d865eecd1c57855
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-01-27 18:36:10 +0200

    Graph traversal. Depth first search with adjacency matrix

--- src/graphs/adjacency_matrix/Graph.java
+++ src/graphs/adjacency_matrix/Graph.java
@@ -3,6 +3,7 @@ package graphs.adjacency_matrix;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Graph {
     List<GraphNode> nodeList = new ArrayList<>();
@@ -70,6 +71,31 @@ public class Graph {
         for(GraphNode node: nodeList){
             if(!node.isVisited()){
                 bfsVisit(node);
+            }
+        }
+    }
+
+    void dfsVisit(GraphNode node){
+        Stack<GraphNode> stack = new Stack<>();
+        stack.push(node);
+        while(!stack.isEmpty()){
+            GraphNode currentNode = stack.pop();
+            currentNode.setVisited(true);
+            System.out.print(currentNode.getName()+" ");
+            ArrayList<GraphNode> neighbors = getNeighbors(currentNode);
+            for(GraphNode neighbor : neighbors){
+                if(!neighbor.isVisited()){
+                    stack.push(neighbor);
+                    neighbor.setVisited(true);
+                }
+            }
+        }
+    }
+
+    void dfs(){
+        for(GraphNode node : nodeList){
+            if(!node.isVisited()){
+                dfsVisit(node);
             }
         }
     }
