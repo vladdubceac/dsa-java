70cc785d0d5ce9272459e240504fa27fe52d1155
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-02-13 21:02:48 +0200

    Divide and conquer. Number factor

---
+++ src/divide_conquer/number_factor/Main.java
@@ -0,0 +1,10 @@
+package divide_conquer.number_factor;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberFactor nf = new NumberFactor();
+        System.out.println(nf.waysTOGetN(4));
+        System.out.println(nf.waysTOGetN(5));
+        System.out.println(nf.waysTOGetN(6));
+    }
+}
---
+++ src/divide_conquer/number_factor/NumberFactor.java
@@ -0,0 +1,17 @@
+package divide_conquer.number_factor;
+
+public class NumberFactor {
+    public int waysTOGetN(int n){
+        if(n==0||n==1||n==2){
+            return 1;
+        }
+        if(n==3){
+            return 2; // {1,1,1} {3}
+        }
+        int sub1 = waysTOGetN(n-1);
+        int sub2 = waysTOGetN(n-3);
+        int sub3 = waysTOGetN(n-4);
+
+        return sub1 + sub2 + sub3;
+    }
+}
