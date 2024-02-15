97ad5df0a33e31f9dbbe832c34d6080599a459bc
Vladimir Dubceac <vladimir.dubceac@bass.md>
2024-02-15 17:01:11 +0200

    Divide and Conquer. Knapsack

---
+++ src/divide_conquer/zero_one_knapsack/Main.java
@@ -0,0 +1,11 @@
+package divide_conquer.zero_one_knapsack;
+
+public class Main {
+    public static void main(String[] args) {
+        ZeroOneKnapsack zeroOneKnapsack = new ZeroOneKnapsack();
+        int[] profits = {31, 26, 17, 72};
+        int[] weights = {3, 1, 2, 5};
+        int maxProfit = zeroOneKnapsack.knapSack(profits, weights, 7);
+        System.out.println(maxProfit);
+    }
+}
